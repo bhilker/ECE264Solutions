464914e362562df4e5c7d59458dce021b758debc
Benjamin R Hilker <bhilker@ecelinux19.ecn.purdue.edu>
2014-02-12 22:13:55 -0500

    finished echo-lite.c about to test

--- PA04/echo-lite.c
+++ PA04/echo-lite.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
-#include <string.h>;
+#include <string.h>
+#include <stdlib.h>
 
+int main(int argc; char **argv)
+{
+	int index=0;
+	for(index=1;i<argc;index++)
+	{
+		printf("%s",argv[index]);
+		if(index != (argc-1)) printf(" ");
+	}
+	printf("\n");
+	return EXIT_SUCCESS;
+}
