447baab0e08004e16286cd858200c6d1f9c0c2f3
Benjamin R Hilker <bhilker@ecelinux14.ecn.purdue.edu>
2014-02-27 18:06:20 -0500

    adding updated answer06.c with completed partitionIncreasing function

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -1,6 +1,33 @@
 #include "answer06.h"
 #include <stdlib.h>
 #include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
+int sumPartition(int *partition, int len)
+{
+	int i;
+	int sum=0;
+	for(i=0;i<len;i++)
+		sum+=partition[i];
+	return sum;
+}
+//This function tests if the partition is increasing
+int isIncreasing(int budget,int *partition,int len)
+{
+	int test=TRUE;
+	int i=1;
+	while(i<len)
+	{
+		if(partition[i]<=partition[i-1])
+			test=FALSE;
+		i++;
+	}
+
+	if(partition[i-1]==budget) test=FALSE;
+
+	return test;
+}
 
 void printPartition(int *partition, int length)
 {
@@ -18,9 +45,36 @@ void partitionAllHelper(int budget,int *partition,int pos)
 {
 	//Base Case
 	if(budget==0) printPartition(partition,pos);
+
+	//Recursive Case
+	int spending=0;
+	for(spending=1;spending<=budget;spending++)
+	{
+		partition[pos]=spending;
+		partitionAllHelper(budget-spending,partition,pos+1);
+	}
 }
 
-void partitionAll(int value);
+void partitionIncreasingHelper(int budget,int *partition, int pos)
+{
+	//Base Case
+	if(budget==0) printPartition(partition,pos);
+
+	//Recursive Case
+	int spending=0;
+	for(spending=1;spending<=budget;spending++)
+	{
+
+		if(isIncreasing(budget,partition,pos) && budget-sumPartition(partition,pos)>=0)
+		{
+			partition[pos]=spending;
+			partitionIncreasingHelper(budget-spending,partition,pos+1);
+		}
+	}
+
+}
+
+void partitionAll(int value)
 {
 	int *partition=malloc(sizeof(int)*value);
 	partitionAllHelper(value,partition,0);
@@ -29,7 +83,9 @@ void partitionAll(int value);
 
 void partitionIncreasing(int value)
 {
-
+	int *partition=malloc(sizeof(int)*value);
+	partitionIncreasingHelper(value,partition,0);
+	free(partition);
 }
 
 void partitionDecreasing(int value)
